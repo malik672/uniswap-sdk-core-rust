27ab61b94ffa80c3c03c27d7bd1c4d4799abbb14
Shuhui Luo <shuhui@aperture.finance>
2023-12-24 19:11:52 -0800

    test `Price`

--- src/entities/fractions/price.rs
+++ src/entities/fractions/price.rs
@@ -176,3 +176,106 @@ where
         self.as_fraction().multiply(&self.meta.scalar)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::entities::{base_currency::BaseCurrency, currency::Currency, token::Token};
+    use lazy_static::lazy_static;
+
+    const ADDRESS_ZERO: &str = "0x0000000000000000000000000000000000000000";
+    const ADDRESS_ONE: &str = "0x0000000000000000000000000000000000000001";
+
+    lazy_static! {
+        static ref TOKEN0: Currency = Currency::Token(Token::new(
+            1,
+            ADDRESS_ZERO.to_string(),
+            18,
+            None,
+            None,
+            None,
+            None,
+        ));
+        static ref TOKEN0_6: Currency = Currency::Token(Token::new(
+            1,
+            ADDRESS_ZERO.to_string(),
+            6,
+            None,
+            None,
+            None,
+            None,
+        ));
+        static ref TOKEN1: Currency = Currency::Token(Token::new(
+            1,
+            ADDRESS_ONE.to_string(),
+            18,
+            None,
+            None,
+            None,
+            None,
+        ));
+    }
+
+    #[test]
+    fn test_constructor_array_format_works() {
+        let price = Price::new(TOKEN0.clone(), TOKEN1.clone(), 1, 54321);
+        assert_eq!(price.to_significant(5, Rounding::RoundDown), "54321");
+        assert!(price.meta.base_currency.equals(&TOKEN0.clone()));
+        assert!(price.meta.quote_currency.equals(&TOKEN1.clone()));
+    }
+
+    #[test]
+    fn test_constructor_object_format_works() {
+        let price = Price::from_currency_amounts(
+            CurrencyAmount::from_raw_amount(TOKEN0.clone(), 1),
+            CurrencyAmount::from_raw_amount(TOKEN1.clone(), 54321),
+        );
+        assert_eq!(price.to_significant(5, Rounding::RoundDown), "54321");
+        assert!(price.meta.base_currency.equals(&TOKEN0.clone()));
+        assert!(price.meta.quote_currency.equals(&TOKEN1.clone()));
+    }
+
+    #[test]
+    fn test_quote_returns_correct_value() {
+        let price = Price::new(TOKEN0.clone(), TOKEN1.clone(), 1, 5);
+        assert!(price
+            .quote(CurrencyAmount::from_raw_amount(TOKEN0.clone(), 10))
+            .equal_to(&CurrencyAmount::from_raw_amount(TOKEN1.clone(), 50)));
+    }
+
+    #[test]
+    fn test_to_significant_no_decimals() {
+        let p = Price::new(TOKEN0.clone(), TOKEN1.clone(), 123, 456);
+        assert_eq!(p.to_significant(4, Rounding::RoundDown), "3.707");
+    }
+
+    #[test]
+    fn test_to_significant_no_decimals_flip_ratio() {
+        let p = Price::new(TOKEN0.clone(), TOKEN1.clone(), 456, 123);
+        assert_eq!(p.to_significant(4, Rounding::RoundDown), "0.2697");
+    }
+
+    #[test]
+    fn test_to_significant_with_decimal_difference() {
+        let p = Price::new(TOKEN0_6.clone(), TOKEN1.clone(), 123, 456);
+        assert_eq!(
+            p.to_significant(4, Rounding::RoundDown),
+            "0.000000000003707"
+        );
+    }
+
+    #[test]
+    fn test_to_significant_with_decimal_difference_flipped() {
+        let p = Price::new(TOKEN0_6.clone(), TOKEN1.clone(), 456, 123);
+        assert_eq!(
+            p.to_significant(4, Rounding::RoundDown),
+            "0.0000000000002697"
+        );
+    }
+
+    #[test]
+    fn test_to_significant_with_decimal_difference_flipped_base_quote_flipped() {
+        let p = Price::new(TOKEN1.clone(), TOKEN0_6.clone(), 456, 123);
+        assert_eq!(p.to_significant(4, Rounding::RoundDown), "269700000000");
+    }
+}
