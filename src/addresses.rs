2e8976449099c084444b3021098d4566adac06b2
malik <aremumalik05@gmail.com>
2024-04-27 07:16:07 +0100

    Update addresses.rs

--- src/addresses.rs
+++ src/addresses.rs
@@ -175,7 +175,7 @@ lazy_static! {
 }
 
 lazy_static! {
-    /// The `MAINNET_ADDRESSES` struct holds the Uniswap contract addresses for the Goerli Testnet.
+    /// The `GOERLI_ADDRESSES` struct holds the Uniswap contract addresses for the Goerli Testnet.
     pub static ref GOERLI_ADDRESSES: ChainAddresses = {
         ChainAddresses {
             v1_mixed_route_quoter_address: Some(address!(
