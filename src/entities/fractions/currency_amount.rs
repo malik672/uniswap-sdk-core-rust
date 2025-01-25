eb7f38b202826fae59dc7005c5892c83454ff82a
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2025-01-25 15:53:46 -0500

    feat: add `wrapped_owned` method to `CurrencyAmount`
    Updated Cargo.toml to version 3.4.0 and upgraded the `bigdecimal` dependency to version 0.4.7. Added a new `wrapped_owned` method in `CurrencyAmount` for better handling of wrapped currencies.

--- src/entities/fractions/currency_amount.rs
+++ src/entities/fractions/currency_amount.rs
@@ -150,6 +150,16 @@ impl<T: BaseCurrency> CurrencyAmount<T> {
             self.denominator().clone(),
         )
     }
+
+    /// Wrap the currency amount if the currency is not native
+    #[inline]
+    pub fn wrapped_owned(&self) -> Result<CurrencyAmount<Token>, Error> {
+        CurrencyAmount::from_fractional_amount(
+            self.currency.wrapped().clone(),
+            self.numerator().clone(),
+            self.denominator().clone(),
+        )
+    }
 }
 
 #[cfg(test)]
